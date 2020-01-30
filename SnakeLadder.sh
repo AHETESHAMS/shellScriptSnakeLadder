c3d22c9918833559ac0594239a675371268d57c1
AHETESHAMS <aheteshams007@gmail.com>
2020-01-30 17:19:31 +0530

    Added: Sanke and Ladder with two players

--- SnakeLadder.sh
+++ SnakeLadder.sh
@@ -1,42 +1,69 @@
-#!/bin/bash 
-position=0
-i=0
-snake1=6
-snake2=3
-snake3=8
-numberOfTimesDicePlayed=0
-while [[ $position -lt 100 ]]
+#!/bin/bash -x
+playerTurn=0
+player1Position=0
+player2Position=0
+pythonSnake1=6
+pythonSnake2=9
+pythonSnake3=36
+cobraSnake1=17
+cobraSnake2=89
+anacondaSnake1=25
+anacondaSnake2=42
+anacondaSnake3=77
+anacondaSnake4=99
+while [[ $player1Position -lt 100 && $player2Position -lt 100 ]]
 do
-	if [[ position -le 0 ]]
+	randonCheck=$((RANDOM%7))
+	if  [[ $playerTurn -eq 0 ]]
 	then
-		position=0
-	fi
-	diceCheck=$((RANDOM%7))
-	(( numberOfTimesDicePlayed++ ))
-	if [[ $diceCheck -eq $snake1 ]]
-	then
-		position=$(( position-snake ))
-		echo "Down With Snake 1, Position got down by"$snake1
-	elif [[ $diceCheck -eq $snake2 ]]
-	then
-		position=$(( position-snake2 ))
-      echo "Down With Snake 2, Position got down by"$snake2
-	elif [[ $diceCheck -eq $snake3 ]]
-	then
-		position=$(( position-snake3 ))
-      echo "Down With Snake 3, Position got down by"$snake3
-	elif [[ $position -gt $snake1 ]]
-	then
-		snake1=$(( RANDOM%position*2 ))
-		snake2=$(( RANDOM%position*2 ))
-		snake3=$(( RANDOM%position*2 ))
-	else
-		position=$(( position+diceCheck ))
-		if [[ $position -gt 100 ]]
-		then 
-			position=100
+		player1Position=$(( player1Position+randonCheck ))
+		if [[ $player1Position  -eq $pythonSnake1 || $player1Position  -eq $pythonSnake2 || $player1Position  -eq $pythonSnake3 ]]
+		then
+			player1Position=$(( player1Position-pythonSnake1*2 ))
+		elif [[ $player1Position -eq $cobraSnake1 || $player1Position -eq $cobraSnake2 ]]
+		then
+			 player1Position=$(( player1Position-pythonSnake1*2 ))
+		elif [[ $player1Position -eq $anacondaSnake1 || $player1Position -eq $anacondaSnake2 || $player1Position -eq $anacondaSnake3 ||$player1Position -eq $anacondaSnake4 ]]
+		then
+			player1Position=0
 		fi
-		echo "Position Incremented, Now Position is "$position
+		if [[ $player1Position -lt 0 ]]
+		then
+			player1Position=0
+		elif [[ $player1Position -gt 100 ]]
+		then
+			player1Position=100
+		fi
+		playerTurn=1
+	else
+		player2Position=$(( player2Position+randonCheck ))
+		if [[ $playe2Position  -eq $pythonSnake1 || $player2Position  -eq $pythonSnake2 || $player2Position  -eq $pythonSnake3 ]]
+      	then
+         	player2Position=$(( player2Position-pythonSnake1*2 ))
+      elif [[ $player2Position -eq $cobraSnake1 || $player2Position -eq $cobraSnake2 ]]
+      	then
+         	 player2Position=$(( player2Position-pythonSnake1*2 ))
+      elif [[ $player2Position -eq $anacondaSnake1 || $player2Position -eq $anacondaSnake2 || $player2Position -eq $anacondaSnake3 ||$player2Position -eq $anacondaSnake4 ]]
+      	then
+         	player2Position=0
+      fi
+      if [[ $player2Position -lt 0 ]]
+      then
+         player2Position=0
+      elif [[ $player2Position -gt 100 ]]
+      then
+         player2Position=100
+      fi
+
+		playerTurn=0
 	fi
+(( i++ ))
 done
+if [[ $player1Position -gt $player2Position ]]
+then
+	echo "Player 1 Won"
+elif [[ $player2Position -gt $player1Position ]]
+then
+	echo "Player 2 Won"
+fi
 
